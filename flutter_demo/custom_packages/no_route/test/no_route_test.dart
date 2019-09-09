bb5eac646902fc2ac829e1af8111c154065bda0a
GJS <1353990812@qq.com>
2019-09-09 13:27:30 +0800

    unit test

--- flutter_demo/custom_packages/no_route/test/no_route_test.dart
+++ flutter_demo/custom_packages/no_route/test/no_route_test.dart
@@ -1,13 +1,71 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:no_route/no_route.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  // test('adds one to input values', () {
+  //   final calculator = Calculator();
+  //   expect(calculator.addOne(2), 3);
+  //   expect(calculator.addOne(-7), -6);
+  //   expect(calculator.addOne(0), 1);
+  //   expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  // });
+  testWidgets('my first widget test', (WidgetTester tester) async {
+    // You can use keys to locate the widget you need to test
+    var sliderKey = new UniqueKey();
+    var value = 0.0;
+
+    // Tells the tester to build a UI based on the widget tree passed to it
+    await tester.pumpWidget(
+      new StatefulBuilder(
+        builder: (BuildContext context, StateSetter setState) {
+          return new MaterialApp(
+            home: new Material(
+              child: new Center(
+                child: new Slider(
+                  key: sliderKey,
+                  value: value,
+                  onChanged: (double newValue) {
+                    setState(() {
+                      value = newValue;
+                    });
+                  },
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+    expect(value, equals(0.0));
+
+    /// Print a string representation of the currently running app.
+    debugDumpApp();
+
+    // Taps on the widget found by key
+    await tester.tap(find.byKey(sliderKey));
+
+    // Verifies that the widget updated the value correctly
+    expect(value, equals(0.5));
+  });
+
+  testWidgets('test widget NoRoute', (WidgetTester tester) async {
+
+    // Tells the tester to build a UI based on the widget tree passed to it
+    await tester.pumpWidget(
+      new StatefulBuilder(
+        builder: (BuildContext context, StateSetter setState) {
+          return new MaterialApp(
+            home: new Material(
+              child: new NoRoute(),
+            ),
+          );
+        },
+      ),
+    );
+
+    /// Print a string representation of the currently running app.
+    debugDumpApp();
   });
 }
